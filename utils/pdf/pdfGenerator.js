8031f1ca24c635242d5f173205f288299df7cbe7
palash2397 <palashpatil55@gmail.com>
2026-01-21 17:36:47 +0530

    Configure Puppeteer for production with Chromium browser settings

--- utils/pdf/pdfGenerator.js
+++ utils/pdf/pdfGenerator.js
@@ -2,17 +2,21 @@ import puppeteer from "puppeteer";
 import { buildNotesHtmlTemplate } from "./pdfTemplate.js";
 
 export const generatePdfFromHtml = async ({ title, subtitle, html }, res) => {
-  const fullHtml = buildNotesHtmlTemplate({ title, subtitle, html });
-
   const browser = await puppeteer.launch({
     headless: "new",
+    executablePath: "/usr/bin/chromium-browser",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--no-zygote",
+    ],
   });
 
   const page = await browser.newPage();
 
-  await page.setContent(fullHtml, {
-    waitUntil: "networkidle0",
-  });
+  await page.setContent(html, { waitUntil: "networkidle0" });
 
   const pdfBuffer = await page.pdf({
     format: "A4",
@@ -20,7 +24,7 @@ export const generatePdfFromHtml = async ({ title, subtitle, html }, res) => {
     margin: {
       top: "30px",
       right: "30px",
-      bottom: "50px",
+      bottom: "40px",
       left: "30px",
     },
   });
